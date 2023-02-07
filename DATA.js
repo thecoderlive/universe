aa8475a0043e7fdb25feefc4636c3b1e6129786a
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-07 18:24:39 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,31 @@
+export const item = {
+    "id": "36b4b999-57df-4994-9201-aff08afadb40",
+    "question": "Direction north edge country dark Congress she major. Audience general along someone behavior.",
+    "sites": [
+        {
+            "id": "9130807b-18a8-4345-b1ed-d2d8a43d342c",
+            "site_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png",
+            "site_name": "man"
+        },
+        {
+            "id": "2c02282c-62e4-4521-9848-6f083db710ca",
+            "site_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/154.png",
+            "site_name": "fill"
+        },
+        {
+            "id": "e1463f7e-e603-4f78-a49f-79cd38b48c06",
+            "site_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png",
+            "site_name": "response"
+        },
+        {
+            "id": "1cf5d9b4-9943-451b-b3cd-d1794643e36a",
+            "site_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png",
+            "site_name": "nation"
+        },
+        {
+            "id": "03e80147-1bea-42cf-a6bb-a61ade318622",
+            "site_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png",
+            "site_name": "onto"
+        }
+    ]
+}
